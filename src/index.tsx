f0cee4fe7fd21e3bb11231471dc083ca36c440c8
SHERlocked93 <443033099@qq.com>
2020-11-04 16:33:52 +0800

    deploy: 部署文件修改+3

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom'
 import moment from 'moment'
 import { ConfigProvider } from 'antd'
 
-import 'normalize.css/normalize.css'
 import 'antd/dist/antd.css'
 import './index.css'
 import App from './App'
